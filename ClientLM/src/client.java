b40ef856f44b2789cb633006ed3505e2c0e03b1c
MarcHunter87 <marchunter888@gmail.com>
2025-03-16 20:06:26 +0100

    funciona

--- ClientLM/src/client.java
+++ ClientLM/src/client.java
@@ -3,48 +3,77 @@ import java.io.*;
 import java.util.Scanner;
 
 public class client {
-	
-	private static String keyword = "bye";
-	private static int port = 1234;
-	
-	public static void main(String[] args) throws IOException {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Client chat to port" + port + "\n");
-		
-		Socket s;
-		
-		try {
-			s = new Socket("localhost", port);
-			System.out.println("Inicializing Client: OK!\n");
-		} catch (IOException e) {
-			System.out.println("Inicializing Client: " + e.getMessage() + "\n");
-			return;
-		}
-		
-		PrintWriter pr; 
-		
-		try {
-			pr = new PrintWriter(s.getOutputStream());
-			System.out.println("Inicializing Chat: OK!\n");
-		} catch (IOException e) {
-			System.out.println("Inicializing Chat: " + e.getMessage() + "\n");
-			return;
-		}
-			
-		InputStreamReader in = new InputStreamReader(s.getInputStream());
+
+    private static String keyword = "bye";
+    private static int port = 1234;
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Client chat to port " + port + "\n");
+
+        Socket s;
+        try {
+            s = new Socket("localhost", port);
+            System.out.println("Inicializing Client: OK!\n");
+        } catch (IOException e) {
+            System.out.println("Inicializing Client: " + e.getMessage() + "\n");
+            return;
+        }
+
+        PrintWriter pr;
+        try {
+            pr = new PrintWriter(s.getOutputStream());
+            System.out.println("Inicializing Chat: OK!\n");
+        } catch (IOException e) {
+            System.out.println("Inicializing Chat: " + e.getMessage() + "\n");
+            return;
+        }
+
+        InputStreamReader in = new InputStreamReader(s.getInputStream());
         BufferedReader bf = new BufferedReader(in);
-            
+
         String str;
         while (true) {
             System.out.print("Client: ");
             str = sc.nextLine();
+
+            if (str.equalsIgnoreCase(keyword) || str.equalsIgnoreCase("end")) {
+                System.out.println("\nKeyword Detected!");
+                break;
+            }
+
             pr.println(str);
             pr.flush();
-            
+
             str = bf.readLine();
+
+            if (str == null) {
+                System.out.println("\nKeyword Detected!");
+                break;
+            }
+
             System.out.println("\nServer: " + str + "\n");
         }
+
+        try {
+			sc.close();
+			pr.close();
+			in.close();
+            bf.close();
+            System.out.println("\nClosing Chat: OK");
+        } catch (IOException e) {
+            System.out.println("\nClosing Chat: " + e.getMessage() + "\n");
+        }
+
+        try {
+            s.close();
+            System.out.println("\nClosing Client: OK");
+        } catch (IOException e) {
+            System.out.println("\nClosing Client: " + e.getMessage());
+        }
+
+		System.out.println("\nBye!");
     }
 }
--- ServerLM/src/server.java
+++ ServerLM/src/server.java
@@ -4,51 +4,80 @@ import java.util.Scanner;
 
 public class server {
 
-	private String keyword = "end";
-	private static int port = 1234;
-	
-	public static void main(String[] args) throws IOException {
-		
-		Scanner sc = new Scanner(System.in);
+    private static String keyword = "end";
+    private static int port = 1234;
 
-		System.out.println("Server chat at port " + port + "\n");
-		
-		System.out.println("Inicializing Server: OK!\n");
-		
-		ServerSocket ss = new ServerSocket(port);
-		
-		Socket s;
-		
-		try {
-			s = ss.accept();
-			System.out.println("Connection from Client: OK!\n");
-		} catch (IOException e) {
-			System.out.println("Connection from Client: " + e.getMessage() + "\n");
-			return; 
-		}
-		
-		InputStreamReader in;
-		
-		try {
-			 in = new InputStreamReader(s.getInputStream());
-			 System.out.println("Inicializing Chat: OK\n");
-		} catch (IOException e) {
-				System.out.println("Inicializing Chat: " + e.getMessage() + "\n");
-				return; 
-		}
+    public static void main(String[] args) throws IOException {
 
-		BufferedReader bf = new BufferedReader(in);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Server chat at port " + port + "\n");
+        System.out.println("Inicializing Server: OK!\n");
+
+        ServerSocket ss = new ServerSocket(port);
+        Socket s;
+
+        try {
+            s = ss.accept();
+            System.out.println("Connection from Client: OK!\n");
+        } catch (IOException e) {
+            System.out.println("Connection from Client: " + e.getMessage() + "\n");
+            return;
+        }
+
+        InputStreamReader in;
+        try {
+            in = new InputStreamReader(s.getInputStream());
+            System.out.println("Inicializing Chat: OK");
+        } catch (IOException e) {
+            System.out.println("Inicializing Chat: " + e.getMessage());
+            return;
+        }
+
+        BufferedReader bf = new BufferedReader(in);
         PrintWriter pr = new PrintWriter(s.getOutputStream());
-        
+
         String str;
         while (true) {
             str = bf.readLine();
+
+            if (str == null) {
+                System.out.println("\nKeyword Detected!");
+                break;
+            }
+
             System.out.println("\nClient: " + str + "\n");
-            
+
             System.out.print("Server: ");
             str = sc.nextLine();
+
+            if (str.equalsIgnoreCase(keyword) || str.equalsIgnoreCase("bye")) {
+                System.out.println("\nKeyword Detected!");
+                break;
+            }
+			
             pr.println(str);
             pr.flush();
         }
-    }  
+
+        try {
+			sc.close();
+            in.close();
+			bf.close();
+            pr.close();
+            System.out.println("\nClosing Chat: OK");
+        } catch (IOException e) {
+            System.out.println("\nClosing Chat: " + e.getMessage() + "\n");
+        }
+
+        try {
+			ss.close();
+            s.close();
+            System.out.println("\nClosing Server: OK");
+        } catch (IOException e) {
+            System.out.println("\nClosing Server: " + e.getMessage());
+        }
+		
+		System.out.println("\nBye!");
+    }
 }
